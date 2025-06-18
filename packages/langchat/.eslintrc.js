6d4fa06243a6e72240d46779f3dd4920343c4992
alpha-xone <alpha.xone@outlook.com>
2025-06-18 11:34:20 +0800

    fix lint

---
+++ packages/langchat/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: [
+    'expo',
+    '@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'prefer-const': 'warn',
+  },
+  ignorePatterns: [
+    'node_modules/',
+    'dist/',
+    '*.js',
+    '*.d.ts'
+  ],
+};
